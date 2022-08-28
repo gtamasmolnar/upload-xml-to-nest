421778673b4c54db886c67cc039ebc5e464b03eb
Tamas Molnar <gtamas.molnar@yahoo.co.uk>
2022-08-28 06:39:59 +0200

    create-user.dto updated

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,12 @@
+import { IsBoolean, IsEmail, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
   name: string;
+  @IsEmail()
   email: string;
+  @IsString()
   password: string;
+  @IsBoolean()
   isActive: boolean;
 }
