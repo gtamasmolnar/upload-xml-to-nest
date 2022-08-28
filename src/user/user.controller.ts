16a90e936e20893788b5f36364ef87bd4053a08c
Tamas Molnar <gtamas.molnar@yahoo.co.uk>
2022-08-28 06:59:00 +0200

    + create user route + store() 2 usr.ctrlr

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Post()
+  store(@Body() createUserDto: CreateUserDto) {
+    return this.userService.store(createUserDto);
+  }
 }
